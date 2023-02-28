8c230ab224154fcedf59f703fe17d526d8f436fd
SDW <stephenoilesanmi@gmail.com>
2023-02-28 16:46:35 +0000

    set up connection to AWS and Postgres

---
+++ migration-pipelines/main.py
@@ -0,0 +1,22 @@
+import boto3
+from dotenv import load_dotenv
+from configparser import ConfigParser
+import os 
+import json
+import pandas as pd
+import io
+import psycopg2
+from sqlalchemy import create_engine
+from pathlib import Path
+
+
+
+# Set up connection to AWS 
+
+
+
+
+# Set up connection to Postgres database 
+
+
+
