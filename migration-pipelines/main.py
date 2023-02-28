62e44d61187a8133d93adb6a20d1cce201f7f761
SDW <stephenoilesanmi@gmail.com>
2023-02-28 17:04:46 +0000

    added logger objects

--- migration-pipelines/main.py
+++ migration-pipelines/main.py
@@ -1,22 +1,68 @@
 import boto3
 from dotenv import load_dotenv
-from configparser import ConfigParser
 import os 
 import json
 import pandas as pd
 import io
 import psycopg2
+import logging, coloredlogs
 from sqlalchemy import create_engine
 from pathlib import Path
 
+# ================================================ LOGGER ================================================
 
 
+# Set up root root_logger 
+root_logger     =   logging.getLogger(__name__)
+root_logger.setLevel(logging.DEBUG)
+
+
+# Set up formatter for logs 
+file_handler_log_formatter      =   logging.Formatter('%(asctime)s  |  %(levelname)s  |  %(message)s  ')
+console_handler_log_formatter   =   coloredlogs.ColoredFormatter(fmt    =   '%(message)s', level_styles=dict(
+                                                                                                debug           =   dict    (color  =   'white'),
+                                                                                                info            =   dict    (color  =   'green'),
+                                                                                                warning         =   dict    (color  =   'cyan'),
+                                                                                                error           =   dict    (color  =   'red',      bold    =   True,   bright      =   True),
+                                                                                                critical        =   dict    (color  =   'black',    bold    =   True,   background  =   'red')
+                                                                                            ),
+
+                                                                                    field_styles=dict(
+                                                                                        messages            =   dict    (color  =   'white')
+                                                                                    )
+                                                                                    )
+
+
+# Set up file handler object for logging events to file
+current_filepath    =   Path(__file__).stem
+file_handler        =   logging.FileHandler('logs/L4_dwh_layer/user_access_layer/' + current_filepath + '.log', mode='w')
+file_handler.setFormatter(file_handler_log_formatter)
+
+
+# Set up console handler object for writing event logs to console in real time (i.e. streams events to stderr)
+console_handler     =   logging.StreamHandler()
+console_handler.setFormatter(console_handler_log_formatter)
+
+
+# Add the file handler 
+root_logger.addHandler(file_handler)
+
+
+# Only add the console handler if the script is running directly from this location 
+if __name__=="__main__":
+    root_logger.addHandler(console_handler)
+
+
+
+
+
+
+# ================================================ CONFIG ================================================
+
+# Load environment variables to session
 load_dotenv()
 
-# Create a config file for storing environment variables
-path    =   os.path.abspath('postgres-to-s3/config.ini')
-config  =   ConfigParser()
-config.read(path)
+
 
 
 
@@ -35,6 +81,27 @@ password                =   os.getenv('PASSWORD')
 postgres_connection     =   None
 cursor                  =   None
 
+sql_alchemy_engine                  =       create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
 
 
 print("SUCCESS!!!")
+
+
+try:
+
+
+    engine = create_engine(f'{sql_alchemy_engine}')
+     
+        # Validate the Postgres database connection
+        if postgres_connection.closed == 0:
+            root_logger.debug(f"")
+            root_logger.info("=================================================================================")
+            root_logger.info(f"CONNECTION SUCCESS: Managed to connect successfully to the {active_db_name} database!!")
+            root_logger.info(f"Connection details: {postgres_connection.dsn} ")
+            root_logger.info("=================================================================================")
+            root_logger.debug("")
+        
+        elif postgres_connection.closed != 0:
+            raise ConnectionError("CONNECTION ERROR: Unable to connect to the demo_company database...") 
+except psycopg2.Error as e:
+    print(e)
